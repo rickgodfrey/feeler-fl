67afe8bd178e7250771fd5fdbe679808e23a815b
stayyoung1991 <stayyoung1991@outlook.com>
2020-04-11 06:23:06 +0800

    improvement

--- Exceptions/Exception.php
+++ Exceptions/Exception.php
@@ -3,8 +3,8 @@
 namespace Feeler\Fl\Exceptions;
 
 class Exception extends \Exception {
-    function __construct($code = 0, $message = "", \Exception $previous = null)
+    public function __construct($code = 0, $message = "", \Exception $previous = null)
     {
-        \Exception::__construct($message, $code, $previous);
+        parent::__construct($message, $code, $previous);
     }
 }
--- File.php
+++ File.php
@@ -14,7 +14,7 @@ class File extends \Feeler\Base\File{
      * @param int $mode
      * @param int $pointer
      * @param bool $override
-     * @throws \Feeler\Base\Exception\InvalidDataDomainException
+     * @throws \Feeler\Base\Exceptions\InvalidDataDomainException
      */
     public function __construct(string $file, int $mode = self::MODE_R, int $pointer = self::POINTER_HEAD, bool $override = false)
     {
