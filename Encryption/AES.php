001ac6af6dcc37c91ed15d530095b816d6cb6f5e
stayyoung1991 <stayyoung1991@outlook.com>
2020-04-25 04:58:30 +0800

    improvement

--- Encryption/AES.php
+++ Encryption/AES.php
@@ -4,14 +4,15 @@ namespace Feeler\Fl\Encryption;
 
 class AES {
     const AES_256_CBC = "AES-256-CBC";
+    const IV = "AJhHaZ6";
 
     public static function encrypt(string $text, string $key) : string
     {
-        return base64_encode(openssl_encrypt($text, self::AES_256_CBC, $key, OPENSSL_RAW_DATA));
+        return base64_encode(openssl_encrypt($text, self::AES_256_CBC, $key, OPENSSL_RAW_DATA, self::IV));
     }
 
     public static function decrypt(string $encode, string $key) : string
     {
-        return openssl_decrypt(base64_decode($encode), self::AES_256_CBC, $key, OPENSSL_RAW_DATA);
+        return openssl_decrypt(base64_decode($encode), self::AES_256_CBC, $key, OPENSSL_RAW_DATA, self::IV);
     }
 }
