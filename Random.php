9b934233f7f6ebee6e93f2c16809f22d4036b8e8
stayyoung1991 <stayyoung1991@outlook.com>
2020-06-07 03:58:40 +0800

    improvement

--- Random.php
+++ Random.php
@@ -15,7 +15,7 @@ class Random{
 	public static function uuid(bool $whole = false): string {
         $uuid = self::UUID_ZONE_FLAG;
 	    if($macAddr = NetworkCard::getEth0MacAddr()){$uuid .= "::".md5($macAddr);}
-        $uuid .= "::".md5(self::string(93, false, false));
+        $uuid .= "::".md5(self::string(92, false, false));
         $uuid .= "::".md5(uniqid(mt_rand((int)((double)Time::microSecond() * 100000000), (int)9223372036854775807), true));
         $uuid = strtolower(substr(sha1($uuid), 0, 32));
 		if($whole){$uuid = substr($uuid, 0, 8) ."-".substr($uuid, 8, 4) ."-".substr($uuid, 12, 4) ."-".substr($uuid, 16, 4) ."-".substr($uuid, 20, 12);}
@@ -31,7 +31,7 @@ class Random{
     public static function string($length, bool $isNumeric = false, bool $withUUID = true) :string {
         $hash = "";
         if($withUUID){$seed = ($seed = Random::uuid()).strtoupper($seed);}
-        else{$seed = str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`1234567890-=~!@#$%^&*()_+[]\;',./{}|:<>?");}
+        else{$seed = str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`1234567890-=~!@#$%^&*()_+[];',?/{}|:<>.");}
         $seed = base_convert($seed, 16, ($isNumeric ? 10 : 36));
         $max = strlen($seed) - 1;
         for($i = 0; $i < $length; $i++) {
