d360d9d2fb838de264e4eebb27477d17e0b5d31d
stayyoung1991 <stayyoung1991@outlook.com>
2020-06-08 02:06:33 +0800

    improvement

--- Random.php
+++ Random.php
@@ -55,10 +55,10 @@ class Random{
      * @param $length
      * @return string
      */
-    public static function number($length, $startWith = null) :string {
+    public static function number($length, bool $strict = true, $startWith = null) :string {
         return (Number::isInteric($startWith) && ($startWith = (int)$startWith) !== null && $startWith >= 0 && $startWith <= 9 && ($numbers = str_replace("{$startWith}", "", "0123456789")))
-            ? (preg_match("/[{$numbers}]*?({$startWith}[0-9]*)?/", ($number = self::number($length)), $matches) && Number::isInteric($matches[1]) ? (($matchedLen = strlen($matches[1])) === $length ? $matches[1] : ($matches[1].self::number($length - $matchedLen))) : self::number($length, $startWith))
-            : (($len = strlen(($number = ltrim(self::string($length, true), "0")))) < $length ? $number.self::string(($length - $len), true) : $number);
+            ? (preg_match("/[{$numbers}]*?({$startWith}[0-9]*)?/", ($number = self::number($length)), $matches) && Number::isInteric($matches[1]) ? (($matchedLen = strlen($matches[1])) === $length ? $matches[1] : ($matches[1].self::number($length - $matchedLen, false))) : self::number($length, $startWith))
+            : (($number = self::string($length, true)) && $strict ? (($len = strlen(($number = ltrim($number, "0")))) < $length ? $number.self::string(($length - $len), true) : $number) : $number);
     }
 
     public static function uniqueId() :?string {
