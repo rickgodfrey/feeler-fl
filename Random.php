08b32d4af65b9b936c5e3fe5ec3439badaebc274
stayyoung1991 <stayyoung1991@outlook.com>
2020-06-07 08:35:05 +0800

    improvement

--- Random.php
+++ Random.php
@@ -12,14 +12,13 @@ use Feeler\Base\Str;
 use Feeler\Fl\Hardware\NetworkCard;
 
 class Random{
-    const BASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`1234567890-=~!@#$%^&*()_+[]\\;',?/{}|:\"<>.";
     const UUID_ZONE_FLAG = "7eb4014b7da8e2ffcbaec069a5b6c87c";
 
     public static function uuid(bool $whole = false): string {
         $uuid = self::UUID_ZONE_FLAG;
         if($macAddr = NetworkCard::getEth0MacAddr()){$uuid .= "::".md5($macAddr);}
-        $uuid .= "::".md5(self::string(92, false, false));
-        $uuid .= "::".md5(uniqid(mt_rand((int)((double)Time::microSecond() * 100000000), (int)9223372036854775807), true));
+        $uuid .= "::".md5(self::string(128, false, false));
+        $uuid .= "::".self::uniqueId();
         $uuid = strtolower(substr(sha1($uuid), 0, 32));
         if($whole){$uuid = substr($uuid, 0, 8) ."-".substr($uuid, 8, 4) ."-".substr($uuid, 12, 4) ."-".substr($uuid, 16, 4) ."-".substr($uuid, 20, 12);}
         return $uuid;
@@ -35,8 +34,8 @@ class Random{
         if(!Number::isPosiInteric($length)){return "";}
         $length = (int)$length;
         $hash = "";
-        if($withUUID){$string = strtoupper(($string = Random::uuid())).$string;}
-        else{$string = self::BASE_CHARS;}
+        if($withUUID){$string = strtoupper(($string = self::uuid())).$string;}
+        else{$string = strtoupper(($string = self::uniqueId())).$string;}
         $seed = base_convert(str_shuffle($string), 16, ($isNumeric ? 10 : 36));
         $max = strlen($seed) - 1;
         while($max < $length){
@@ -55,6 +54,10 @@ class Random{
      * @return string
      */
     public static function number($length) : string {
-        return self::string($length, true);
+        return ($len = strlen(($number = ltrim(self::string($length, true), "0")))) < $length ? $number.self::string(($length - $len), true) : $number;
+    }
+
+    public static function uniqueId(){
+        return md5(uniqid(mt_rand((int)((double)Time::microSecond() * 100000000), (int)9223372036854775807), true));
     }
 }
